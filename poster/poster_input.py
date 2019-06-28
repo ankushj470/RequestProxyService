e9e330fc9850d4c60e18293970a25af26d6cf131
ankush soni <ankush.soni@gaincredit.com>
2019-06-28 04:45:28 +0000

    Adding Poster to post dummy request to the service

---
+++ poster/poster_input.py
@@ -0,0 +1,5 @@
+ClientID = "1"
+URL = "http://192.168.56.101:8000/test"
+Headers = "{'content-type': 'application/json'}"
+Request_Type = 'GET'
+Request_Body = 'A'
---
+++ poster/service_poster.py
@@ -0,0 +1,15 @@
+import requests
+import poster_input
+
+'''
+clientid = 
+url = 
+Headers = raw_input("Please enter the Headers")
+Request_Type = raw_input("Enter the Request Type")
+Request_Body = raw_input("Please Enter the Request Body")
+'''
+
+data = {"ClientID":poster_input.ClientID,"URL":poster_input.URL,"Headers":poster_input.Headers,"Request_Type":poster_input.Request_Type,"Request_Body":poster_input.Request_Body}
+
+
+print requests.post("http://192.168.56.101:5000/proxy_request",json = data).text
